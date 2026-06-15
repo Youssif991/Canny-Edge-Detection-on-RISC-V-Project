521420915f97e0d3eec4750a41c0ad7dcee152db
Youssif991 <youssefx12xx@gmail.com>
2026-06-15 18:28:24 +0300

    feat: implemented the direction function header

--- include/direction.hpp
+++ include/direction.hpp
@@ -8,4 +8,7 @@
 
 #include <cstdint>
 
-// TODO: declare functions for direction stage
+template <typename PixelT = uint8_t, typename GradientT = int16_t>
+Status Direction(const image::io::metadata_t<PixelT> &image,
+                 const GradientT *__restrict Gx,
+                 const GradientT *__restrict Gy);
