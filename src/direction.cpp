588b39aedc31f49a848022f2d67f454666a6ec60
Youssif991 <youssefx12xx@gmail.com>
2026-06-15 19:35:05 +0300

    fix: corrected some typos in the direction implementation

--- src/direction.cpp
+++ src/direction.cpp
@@ -11,17 +11,17 @@ Status Direction(const image::io::metadata_t<PixelT> &image,
                  const GradientT *__restrict Gx,
                  const GradientT *__restrict Gy)
 {
-    if (!input_image.height || !input_image.width || !input_image.buffer)
+    if (!image.height || !image.width || !image.buffer)
     {
-        return input_image.buffer ? Status::E_NOK : Status::E_INVAL_PTR;
+        return image.buffer ? Status::E_NOK : Status::E_INVAL_PTR;
     }
 
     uint8_t angle = 0;
     const uint32_t pixel_count = image.pixel_count;
     for (uint32_t i = 0; i < pixel_count; i++)
     {
-        const GradientT cur_x = gx_ptr[i];
-        const GradientT cur_y = gy_ptr[i];
+        const GradientT cur_x = Gx[i];
+        const GradientT cur_y = Gy[i];
         const uint16_t abs_x = static_cast<uint16_t>(std::abs(cur_x));
         const uint16_t abs_y = static_cast<uint16_t>(std::abs(cur_y));
 
